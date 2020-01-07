69e451b4499d24e190066f1e9b8a6158b1e4ec7b
kulichkov <mikhail.kulichkov@gmail.com>
2020-01-07 15:12:32 +0300

    Initial Commit

---
+++ News.Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News.Demo
+//
+//  Created by Mikhail Kulichkov on 07.01.2020.
+//  Copyright © 2020 Kulichkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
